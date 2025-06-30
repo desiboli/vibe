090497c71074e063ee1447496b4683c49553ad72
desiboli <adam.benals@gmail.com>
2025-07-01 00:08:52 +0200

    trpc setup

---
+++ src/app/client-greeting.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+import { useTRPC } from "@/trpc/client"
+export function ClientGreeting() {
+  const trpc = useTRPC()
+  const greeting = useQuery(trpc.createAI.queryOptions({ text: "Taffo!" }))
+  if (!greeting.data) return <div>Loading...</div>
+  return <div>{greeting.data.greeting}</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Hello World</div>
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { Suspense } from "react"
+import { getQueryClient, trpc } from "@/trpc/server"
+import { ClientGreeting } from "./client-greeting"
+
+export default async function Home() {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({
+      text: "Taffo!",
+    })
+  )
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>Hello World!</div>
+        <ClientGreeting />
+      </Suspense>
+    </HydrationBoundary>
+  )
 }
