7b13cd07b1d605d988f9782c27f4410c736358dc
desiboli <adam.benals@gmail.com>
2025-07-31 23:06:24 +0200

    ai cron jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
 import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
 
 export default function Home() {
+  const [value, setValue] = useState("")
   const trpc = useTRPC()
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,8 +20,9 @@ export default function Home() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        onClick={() => invoke.mutate({ text: "Taffo!" })}
+        onClick={() => invoke.mutate({ value: value })}
         disabled={invoke.isPending}
       >
         Invoke background job
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,28 @@
+import { createAgent, openai } from "@inngest/agent-kit"
+
 import { inngest } from "./client"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // Imagine this is a transcript step
-    await step.sleep("wait-a-moment", "10s")
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      description: "An expert coder which can write and debug code.",
+      system:
+        "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({
+        model: "gpt-4o",
+      }),
+    })
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    )
+
+    console.log(output)
 
     // Imagine this is a summary step
-    await step.sleep("wait-a-moment", "5s")
-    return { message: `Hello ${event.data.email}!` }
+    return { success: "ok!" }
   }
 )
